05dd6f5fb4428917aac646330b6f9f665ba51d91
KeshavSharma19 <115711608+KeshavSharma19@users.noreply.github.com>
2025-07-13 12:51:59 +0530

    Added booking Apis

--- admin/admin.js
+++ admin/admin.js
@@ -1,9 +1,9 @@
 const express = require('express');
 require('dotenv').config();
+const app = express();
 const connectDB = require('../db/conn');
 const morgan = require('morgan'); // ✅ logger
 
-const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
