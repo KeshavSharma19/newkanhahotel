acc9d7df96ab7a04dbf5a9ebc5a4535f99c83f69
shivam <shivamkhandelwal0033@gmail.com>
2025-07-21 12:10:12 +0530

    backend work category and menu

--- admin/routes/menuItemRoutes.js
+++ admin/routes/menuItemRoutes.js
@@ -6,6 +6,6 @@ router.post('/create-item/:categoryId',  itemController.createItem);
 router.get('/menu-items', itemController.getItems);
 router.get('/menu-item-by-categories/:categoryId', itemController.getItemsByCategory);
 router.post('/update-item/:id',  itemController.updateItem);
-router.delete('/delete-item/:id',  itemController.deleteItem);
+router.post('/delete-item/:id',  itemController.deleteItem);
 
 module.exports = router;
